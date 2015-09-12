31a38edba755963a891df4ec8abda6c5ee64355a
Andy Mellor <mellor91@hotmail.co.uk>
2015-09-12 20:52:24 +0100

    Tidy up

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
@@ -14,11 +14,28 @@ db = SQLAlchemy(app)
 def not_found(error):
     return render_template('404.html'), 404
 
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint)
+            links.append((url, rule.endpoint))
+    return '\n'.join([','.join(link) for link in links])
+
+
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.mod_home.controllers import mod_home as home_module
 
 # Register blueprint(s)
-app.register_blueprint(home_module)
+app.register_blueprint(home_module, url_prefix='/home')
 
 # Build the database:
 # This will create the database file using SQLAlchemy
--- app/mod_home/controllers.py
+++ app/mod_home/controllers.py
@@ -2,25 +2,9 @@ from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for, Response, \
                   stream_with_context
 import json
-import os
 
-# Import the database object from the main app module
-from app import db
-
-# Import module forms
-#from app.mod_lisa.forms import SimulationForm, GraphGenerateForm
-
-# Import module models (i.e. User)
-from app.mod_home.models import Publications, Conferences
-
-# Define the blueprint: 'auth', set its url prefix: app.url/auth
-mod_home = Blueprint('home', __name__, url_prefix='/home',
-                         static_folder='static', static_url_path='/static',)
-
-
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-APP_STATIC = os.path.join(APP_ROOT, 'static')
-                         
+# Define the Blueprint
+mod_home = Blueprint('home', __name__, static_folder='static')
 
 @mod_home.route('/')
 def index():
@@ -28,9 +12,8 @@ def index():
 
 @mod_home.route('/vitae/')
 def vitae():
-
-    with open(os.path.join(APP_STATIC,'portfolio.json'),'r') as w:
-        data = json.load(w)
+    with mod_home.open_resource('static/portfolio.json') as w:
+         data = json.load(w)
         
     publications = data['publications']
     conferences = data['conferences']
@@ -39,7 +22,7 @@ def vitae():
     return render_template('/home/vitae.html', publications=publications, conferences=conferences,
                            cv=cv)    
     
-@mod_home.route('/research')
+@mod_home.route('/research/')
 def research():
     return render_template('/home/research.html')   
     
